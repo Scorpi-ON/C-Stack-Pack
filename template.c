d303a15142710af747e4ad0bc1e31a590594e8d1
Scorpi-ON <akkauntov107@gmail.com>
2022-02-02 22:13:37 +0300

    Added a template for tasks.

---
+++ template.c
@@ -0,0 +1,23 @@
+#include "libs/tools.h"
+#include "libs/stack.h"
+
+
+// Your target func.
+
+
+int main(void)
+{
+    setlocale(LC_ALL, "RUS");
+    STACK *myStack; // And maybe another vars.
+
+    puts("Заполните стек:");
+    myStack = create(); // Or fill(&myStack) if you need the stack length.
+    // Maybe additional user input.
+    printf("\nИсходный стек: ");
+    printToFile(myStack, stdout);
+    // Target func call + prints of results to stdout or FILEs.
+
+    // clear() for all used STACKs. 
+    system("pause");
+    return 0;
+}
