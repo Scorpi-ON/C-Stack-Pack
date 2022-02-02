38520eed92329d6369262cea6f6ba4880d409584
Scorpi-ON <akkauntov107@gmail.com>
2022-02-02 23:15:47 +0300

    Added frecuency.c (may need refactoring).

---
+++ frequency.c
@@ -0,0 +1,56 @@
+#include "lib/tools.h"
+#include "lib/stack.h"
+
+
+int getFrequency(STACK *head, int elem)
+{
+    STACK *current = head;
+    int frequency = 0;
+    do
+    {
+        if (current -> value == elem)
+            frequency++;
+    } while (current = current -> next);
+    return frequency;
+}
+
+
+void printFrequenciesIntoFile(STACK *head, FILE *output)
+{
+    STACK *current = head;
+    int currentNum = 0,
+        *usedElems = NULL;
+    bool isUnique;
+    do
+    {
+        isUnique = true;
+        for (int i = 0; i < currentNum; i++)
+            if (current -> value == usedElems[i])
+                isUnique = false;
+        if (isUnique)
+        {
+            fprintf(output, "%d: %d\n", current -> value, getFrequency(head, current -> value));
+            usedElems = realloc(usedElems, (currentNum + 1) * sizeof(int));
+            usedElems[currentNum++] = current -> value;
+        }
+    } while (current = current -> next);
+}
+
+
+int main(void)
+{
+    setlocale(LC_ALL, "RUS");
+    STACK *myStack;
+    FILE *output = fopen("output.txt", "w+");
+
+    puts("Заполните стек:");
+    myStack = create();
+    printf("\nИсходный стек: ");
+    printToFile(myStack, stdout);
+    printFrequenciesIntoFile(myStack, output);
+
+    fclose(output);
+    clear(myStack);
+    system("pause");
+    return 0;
+}
