8e154a903c0e1e0af3206e272da6a54d4c494029
Scorpi-ON <akkauntov107@gmail.com>
2022-02-03 17:35:30 +0300

    Yet another task (seems to be refactored).

---
+++ swap_1st_and_last.c
@@ -0,0 +1,39 @@
+#include "lib/tools.h"
+#include "lib/stack.h"
+
+
+bool swapFirstAndLast(STACK **head)
+{
+    STACK *current = *head,
+          *previous = NULL;
+    int headValue = (*head) -> value;
+    while (current -> next)
+        current = current -> next;
+    if (headValue == current -> value)
+        return false;
+    (*head) -> value = current -> value;
+    current -> value = headValue;
+    return true;
+}
+
+
+int main(void)
+{
+    STACK *myStack;
+    FILE *output = openFile("output.txt", "w");
+    puts("Fill stack:");
+    myStack = create();
+    printf("\nOriginal stack: ");
+    printToFile(myStack, stdout);
+    if (swapFirstAndLast(&myStack))
+    {
+        printf("After swapping: ");
+        printToFile(myStack, stdout);
+        printToFile(myStack, output);
+    }
+    else
+        puts("The first and the last elements are equal! Nothing to change.");
+    clear(myStack);
+    system("pause");
+    return 0;
+}
