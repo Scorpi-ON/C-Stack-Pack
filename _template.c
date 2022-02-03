224fa6868f4259af205b71363785c53e791b95f6
Scorpi-ON <akkauntov107@gmail.com>
2022-02-03 16:15:22 +0300

    Optimized comments.

--- _template.c
+++ _template.c
@@ -2,22 +2,21 @@
 #include "lib/stack.h"
 
 
-// Your target func.
+// Target func.
 
 
 int main(void)
 {
     STACK *myStack;
-    // And maybe another vars.
+    // Possibly another vars.
     FILE *output = openFile("output.txt", "w");
     puts("Fill stack:");
     myStack = create(); // Or fill(&myStack) if you need the stack length.
-    // Maybe additional user input.
+    // Possibly additional user input.
     printf("\nOriginal stack: ");
     printToFile(myStack, stdout);
     // Target func call + prints of results to stdout & output file.
-    clear(myStack);
-    // And for other used STACKs.
+    clear(myStack); // And for other used STACKs.
     system("pause");
     return 0;
 }
